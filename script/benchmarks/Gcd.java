d94c6822a1001a8964f6dfb9bd4764931cc84879
Remi Forax <forax@univ-mlv.fr>
2015-01-04 15:13:54 +0100

    add a benchmark folder

---
+++ script/benchmarks/Gcd.java
@@ -0,0 +1,23 @@
+class Gcd {
+  static int gcd (int a, int b) {
+    while (a != b) {
+      if (a > b) {
+        a = a - b;
+      } else {
+        b = b - a;
+      }
+    }
+    return a;
+  }
+
+  public static void main(String[] args) {
+    int i = 0;
+    int sum = 0;
+    while(i < 100000) {
+      sum = sum + gcd(3, 9);
+      i = i + 1;
+    }
+    System.out.println(sum);
+  }
+}
+
---
+++ script/benchmarks/gcd.js
@@ -0,0 +1,24 @@
+function gcd (a, b) {
+  while (a != b) {
+    if (a > b) {
+      a = a - b
+    } else {
+      b = b - a
+    }
+  }
+  return a;
+}
+
+function main() {
+  i = 0
+  sum = 0
+  while(i < 100000) {
+    sum = sum + gcd(3, 9)
+    i = i + 1
+  }
+  print(sum)
+}
+  
+main()
+
+
